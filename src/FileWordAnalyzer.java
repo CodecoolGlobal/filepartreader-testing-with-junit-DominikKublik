618add8fe31529af9a1c7c15f700b0e324552030
DominikKublik <dominikkrzysztofkublik@gmail.com>
2020-02-24 11:39:26 +0100

    Added methods outline in classes, added exception throwing in setup() method in FilePartReader class

--- src/FileWordAnalyzer.java
+++ src/FileWordAnalyzer.java
@@ -1,2 +1,20 @@
+import java.util.List;
+
 public class FileWordAnalyzer {
+    public FileWordAnalyzer(FilePartReader reader) {
+        this.reader = reader;
+    }
+    FilePartReader reader;
+
+    public List getWordsOrderedAlphabetically(){
+        return null;
+    };
+
+    public List getWordsContainingSubstring(String substring){
+        return null;
+    }
+
+    public List getStringsWhichPalindromes(){
+        return null;
+    }
 }
