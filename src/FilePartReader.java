e8d77c0ff3f7e23d69e50fb28f5665e27b21f248
DominikKublik <dominikkrzysztofkublik@gmail.com>
2020-02-24 11:35:32 +0100

    Added methods outline in classes, added exception throwing in setup() method in FilePartReader class

--- src/FilePartReader.java
+++ src/FilePartReader.java
@@ -1,2 +1,18 @@
 public class FilePartReader {
+    public FilePartReader() {
+    }
+
+    public void setup(String filePath, int fromLine, int toLine){
+        if (toLine < fromLine || fromLine<1){
+            throw new IllegalArgumentException("Wrong method parameters");
+        }
+    }
+
+    public String read(){
+        return null;
+    }
+
+    public String readLines(){
+        return null;
+    }
 }
